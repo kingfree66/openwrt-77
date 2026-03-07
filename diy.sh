7b912f97912f5e0965fcc431f1442607b55bcd47
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2026-03-07 04:54:54 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -81,7 +81,7 @@ grep "GEOIP_VER:=" feeds/packages/net/v2ray-geodata/Makefile || echo "v2ray-geod
 
 ##以下功能按需打开,需要本地编译过带ofa配置的，不要就要修改启用
 #应用过滤
-#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter   
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter   
 
 # ------------------ 替换 luci-app-passwall ------------------
 echo "开始替换 luci-app-passwall 为 Openwrt-Passwall 官方版本..."
